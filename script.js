fa4c44705cc73fd710b48f8c5f5f6a980c0e1865
eskimosa <esskimosa@gmail.com>
2024-10-08 18:24:34 +0200

    worked on creating a loader with JS

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     if (!quote.author) {
         authorText.textContent = 'Unknown';
@@ -20,9 +33,11 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quote.text;
+    complete();
 }
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
