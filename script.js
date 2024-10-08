1f5f7aac9f8ec5610aca9e6866a525b12917638f
eskimosa <esskimosa@gmail.com>
2024-10-08 17:42:32 +0200

    added data fetching via api

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('testing');
+// Get Quotes fro API
+
+let qpiQuotes = [];
+
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json()
+        newQuote();
+    } catch (error) {
+
+    }
+}
+
+// On Load
+getQuotes();
